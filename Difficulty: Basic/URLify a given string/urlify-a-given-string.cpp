922361ee4f60b2d3da7c775ca0f472075b801d4f
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-04-15 00:07:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string URLify(string &s) {
+        // code here
+        string ans="";
+        for(auto &x:s)
+        {
+            if(x==' ')
+            {
+                ans+="%20";
+            }
+            else
+            {
+                ans+=x;
+            }
+        }
+        return ans;
+    }
+};
