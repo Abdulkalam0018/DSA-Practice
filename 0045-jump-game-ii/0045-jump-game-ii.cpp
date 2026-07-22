2a8995018f420e4de55c77542b475eebb3dd62f7
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-07-23 01:16:04 +0530

    Time: 807 ms (5.05%), Space: 21.7 MB (18.46%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+const int INF=1e9+7;
+class Solution {
+public:
+    
+    int jump(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int>dp(n,INF);
+        dp[0]=0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=min(n-1,i+nums[i]);j++)
+            {
+                dp[j]=min(dp[j],1+dp[i]);
+            }
+        }
+
+        return dp[n-1];
+    }
+};
