64a8ab4930cb4191563d9ab8c6a2a9aa72e86b97
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-08-22 04:04:07 +0530

    Time: 54 ms (72.92%), Space: 12 MB (43.56%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,35 +1,53 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
-
     int minCut(string s) {
-        
-        int n=s.size();
-        vector<vector<int>>pal(n,vector<int>(n,0));
+        int n = s.size();
+        if (n <= 1) return 0;
 
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i;j<n;j++)
-            {
-                if(s[i]==s[j] &&(j-i+1<=2 || pal[i+1][j-1]))
-                {
-                    pal[i][j]=1;
+        // 1. Precompute palindrome lookup table in O(N^2)
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+        for (int i = 0; i < n; i++) {
+            pal[i][i] = true;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    if (len == 2) {
+                        pal[i][j] = true;
+                    } else {
+                        pal[i][j] = pal[i + 1][j - 1];
+                    }
                 }
             }
         }
-        vector<int>dp(n+1,INT_MAX);
-        dp[n]=0;
 
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i;j<n;j++)
-            {
-                if(pal[i][j])
-                {
-                    dp[i]=min(1+dp[j+1],dp[i]);
+        // 2. 1D DP to find minimum cuts in O(N^2)
+        // dp[i] = minimum cuts needed for suffix s[i...n-1]
+        vector<int> dp(n, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int minCuts = INT_MAX;
+            for (int j = i; j < n; j++) {
+                if (pal[i][j]) {
+                    if (j == n - 1) {
+                        minCuts = 0; // entire suffix s[i...n-1] is a palindrome
+                    } else {
+                        minCuts = min(minCuts, 1 + dp[j + 1]);
+                    }
                 }
             }
+            dp[i] = minCuts;
         }
-        return dp[0]-1;
 
+        return dp[0];
     }
 };
