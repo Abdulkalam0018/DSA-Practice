ecaecc9740832856ed8c1328bfd7cc687a5eea1d
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-08-29 20:45:35 +0530

    Time: 0 ms (100%), Space: 93.4 MB (77.17%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,69 +1,28 @@
 class Solution {
 public:
-    int maximumScore(vector<int>& nums, int k)
+    int maximumScore(vector<int>& a, int k)
     {
         
-        int n=nums.size();
-        vector<int>pref(n,-1),suf(n,n);
-
-        stack<int>st;
-        st.push(0);
-        for(int i=1;i<n;i++)
-        {
-            while(!st.empty() && nums[st.top()]>=nums[i])
-            {
-                st.pop();
-            }
-            if(!st.empty())
-            {
-                pref[i]=st.top();
-            }
-            else
-            {
-                pref[i]=-1;
-            }
-            st.push(i);
-        }
-
-        while(!st.empty()) st.pop();
-
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--)
-        {
-            while(!st.empty() && nums[st.top()]>=nums[i])
-            {
-                st.pop();
-            }
-            if(!st.empty())
-            {
-                suf[i]=st.top();
-            }
-            else 
-            {
-                suf[i]=n;
-            }
-            st.push(i);
-        }
-
-        for(auto &x:pref)
-        {
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        for(auto &x:suf)
-        {
-            cout<<x<<" ";
-        }
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            int a=pref[i]+1;
-            int b=suf[i]-1;
-            if(k>=a && k<=b)
-            {
-                int dis=b-a+1;
-                int c=dis*nums[i];
-                ans=max(ans,c);
+        int n=a.size();
+        int ans = 0 ;
+        int l = k ;
+        int r = k ;
+        int v = a[k] ;
+        while(true) {
+            ans = max(ans , v * (r - l + 1)) ;
+            if(l == 0 && r == n - 1) break ;
+            if(l - 1 < 0) {
+                v = min(v , a[r + 1]) ;
+                r ++ ;
+            } else if(r + 1 >= n) {
+                v = min(v , a[l - 1]) ;
+                l -- ;
+            } else if(a[l - 1] <= a[r + 1]) {
+                v = min(v , a[r + 1]) ;
+                r ++ ;
+            } else {
+                v = min(v , a[l - 1]) ;
+                l -- ;
             }
         }
         return ans;
