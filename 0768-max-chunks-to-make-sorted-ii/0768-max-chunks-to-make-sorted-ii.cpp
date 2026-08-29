bde493fde553be27f0506dbd90830041d7427e2d
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-08-29 15:41:29 +0530

    Time: 0 ms (100%), Space: 16.2 MB (60.31%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
-    bool fun(int i,unordered_map<int,int>& mp,unordered_map<int,int>&freq)
-    {
-        
-
-
-        for(auto &x:freq)
-        {
-            if(mp.find(x.first)==mp.end() || mp[x.first]!=x.second)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
     int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> suffix_min(n);
         
-        int n=arr.size();
-        int ans=0;
-        vector<int>srt;
-        for(auto &x:arr)
-        {
-            srt.push_back(x);
+        suffix_min[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix_min[i] = min(suffix_min[i + 1], arr[i]);
         }
-        sort(srt.begin(),srt.end());
-
-        unordered_map<int,int>mp;
-        unordered_map<int,int>freq;
-        for(int i=0;i<n;i++)
-        {
-            mp[arr[i]]++;
-            freq[srt[i]]++;
-            bool k=fun(i,mp,freq);
-            //cout<<i<<" "<<k<<endl;
-            if(k)
-            {
+        
+        int ans = 0;
+        int prefix_max = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            prefix_max = max(prefix_max, arr[i]);
+            
+            if (prefix_max <= suffix_min[i + 1]) {
                 ans++;
             }
         }
-        return ans;
+        
+        return ans + 1;
     }
 };
